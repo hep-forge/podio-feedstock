51bad8440f2835adad8b62827dde2577194ab4b4
Marco Meyer-Conde <marco@tcu.ac.jp>
2026-04-15 16:24:44 +0900

    Initial commit

---
+++ recipe/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install conda activate/deactivate scripts
+mkdir -p build-scripts
+cd build-scripts
+cmake "$RECIPE_DIR/scripts"
+cd ..
+
+cmake -S . -B build \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=20 \
+    -DENABLE_SIO=OFF \
+    -DENABLE_JULIA=OFF \
+    -DBUILD_TESTING=OFF \
+    -DENABLE_TESTING=OFF
+
+cmake --build build --parallel "${CPU_COUNT}"
+cmake --install build
